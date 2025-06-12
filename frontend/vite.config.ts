0aa16f586d72a6bffa6bdcdbca067538d416cc9f
ahmedosamasawah <sawahahmedosama@gmail.com>
2025-06-12 06:52:26 +0400

    Initial frontend setup with Svelte, TS, and Vite, Shadcn-Svelte, Tailwind, lucide-Svelte

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import path from "path";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte(), tailwindcss()],
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
+});
